98c289adb0fa82875410f99a8b6bdbb2f01f59b5
Cosmin Stroe <cstroe@users.sf.net>
2016-02-23 08:16:25 -0600

    A word problem.

---
+++ src/chapter/2/2.4.4.ml
@@ -0,0 +1,10 @@
+(* Are (1,2) and (1,2,3) the same type? Are [1,2] and [1,2,3] the same type? 
+
+  (1,2) is int * int
+  (1,2,3) is int * int * int
+
+  No, they are not the same type.
+
+  [1,2] and [1,2,3] are int list, the same type
+
+*)
